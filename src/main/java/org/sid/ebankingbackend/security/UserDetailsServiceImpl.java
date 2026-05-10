31adcad28d7e00becb36bee942d3e85eec5bb1ba
Sana <sana.timourti-etu@etu.univh2c.ma>
2026-05-10 14:59:43 +0100

    commit10

--- src/main/java/org/sid/ebankingbackend/security/UserDetailsServiceImpl.java
+++ src/main/java/org/sid/ebankingbackend/security/UserDetailsServiceImpl.java
@@ -6,11 +6,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-/**
- * Charge les utilisateurs pour Spring Security.
- * TODO : Remplace les utilisateurs en dur par une vraie recherche en base (CustomerRepository, etc.)
- */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
